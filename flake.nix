92f502b0fa249b8019a6c64b52fd3327b0b86fa7
Archer <89430358+AD-Archer@users.noreply.github.com>
2025-02-11 00:13:11 -0500

    Update for homebrew

--- flake.nix
+++ flake.nix
@@ -1,94 +1,99 @@
 {
   description = "My Default system flake";
-  
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
   };
 
-  outputs = { self, nix-darwin, nixpkgs }:
+  outputs = { self, nix-darwin, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, ... }:
   let
-    system = "aarch64-darwin"; # Define the system architecture
+    system = "aarch64-darwin";
     pkgs = nixpkgs.legacyPackages.${system};
-    configuration = { pkgs, config, ... }: {
 
+    configModule = { config, pkgs, ... }: {
       nixpkgs.config.allowUnfree = true;
+
       environment.systemPackages = [
         pkgs.neovim
         pkgs.neofetch
         pkgs.git
-        pkgs.neofetch
         pkgs.curl
-        pkgs.mkalias
         pkgs.spotify
-        pkgs.raycast
         pkgs.vscode
         pkgs.zoom-us
         pkgs.discord
         pkgs.appcleaner
-
-
-
-
-
-
       ];
 
-
-      # alais
-      system.activationScripts.applications.text = let
-  env = pkgs.buildEnv {
-    name = "system-applications";
-    paths = config.environment.systemPackages;
-    pathsToLink = "/Applications";
-  };
-in
-  pkgs.lib.mkForce ''
-  # Set up applications.
-  echo "setting up /Applications..." >&2
-  rm -rf /Applications/Nix\ Apps
-  mkdir -p /Applications/Nix\ Apps
-  find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-  while read -r src; do
-    app_name=$(basename "$src")
-    echo "copying $src" >&2
-    ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-  done
-      '';
-      
-
-      nix.settings.experimental-features = "nix-command flakes";
-
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-      system.stateVersion = 6;
-
-      
-
-      # System services
-      services = {
-        # SSH service
-        openssh.enable = true;
-        
-       
+      system.activationScripts.applications = {
+        text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
+          };
+        in ''
+          echo "Setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "Copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
       };
 
+      nix.settings.experimental-features = "nix-command flakes";
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.stateVersion = 6;
+      services.openssh.enable = true;
       security.pam.enableSudoTouchIdAuth = true;
-
     };
 
-    
   in {
-    darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
-      system = system; # <-- This is the crucial missing line!
-      modules = [ configuration ];
+    darwinConfigurations.mac = nix-darwin.lib.darwinSystem {
+      inherit system;
+      modules = [
+        configModule
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "archer";
+            autoMigrate = true;
+            taps = {
+              "homebrew/core" = homebrew-core;
+              "homebrew/cask" = homebrew-cask;
+              "homebrew/bundle" = homebrew-bundle;
+            };
+          };
+        }
+      ];
     };
 
-  
-  
     apps.${system}.darwin-rebuild = {
       type = "app";
-      program = "${pkgs.darwin.darwin-rebuild}/bin/darwin-rebuild";
+      program = "${nix-darwin.legacyPackages.${system}.darwin-rebuild}/bin/darwin-rebuild";
     };
   };
 }
